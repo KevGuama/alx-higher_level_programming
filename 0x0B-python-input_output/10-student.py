e915d137d0fa28476c966b338844ef5e63a16b3e
Dev <kevguama@gmail.com>
2023-07-12 02:14:28 +0300

    QnAll_debug_8.0

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""initializate
-"""
+"""initializate"""
 
 
 class Student:
-    """Class Student
-    """
+    """create class student"""
 
     def __init__(self, first_name, last_name, age):
-        """initialize method
+        """initializate atrributes
+        :param first_name:
+        :param last_name:
+        :param age:
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -24,8 +25,3 @@ class Student:
             if hasattr(self, i):
                 dic[i] = getattr(self, i)
         return dic
-
-    def reload_from_json(self, json):
-        for k, v in json.items():
-            if hasattr(self, k):
-                setattr(self, k, v)
