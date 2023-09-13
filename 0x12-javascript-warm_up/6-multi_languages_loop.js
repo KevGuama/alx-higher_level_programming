fb1d06c86cfb2852006d5b8b8515f1982bb5e78d
Dev <kevguama@gmail.com>
2023-09-14 00:16:38 +0300

    debug_6.0

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const arg1 = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < arg1.length; i++) {
-  console.log(arg1[i]);
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
 }
+
